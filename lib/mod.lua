2da9b7c2c400c41ae9250df3c21dc6f2f445cbfd
Alexander Beels <alex@beels.net>
2023-07-29 21:25:31 -0400

    WIP

--- lib/mod.lua
+++ lib/mod.lua
@@ -193,14 +193,6 @@ end)
 
 local m = {}
 
-paramset = require 'core/paramset'
-
-m.params = paramset.new("modmenu", "Mod Menu")
-m.params:add_number("a", "num widgets", 3, 7, 5)
-m.params:add_number("b", "num buckets", 12, 24, 18)
-
-m.haha = "hoho"
-
 m.key = function(n, z)
   if n == 2 and z == 1 then
     -- return to the mod selection menu
@@ -235,12 +227,15 @@ m.redraw = function()
 
   screen.level(15)
   local o = 10
-  for k,i in pairs(m.params:list()) do
-      screen.move(  0, o + 10 * i)
-      screen.text(m.params:get_name(i))
-      screen.move(127, o + 10 * i)
-      screen.text_right(m.params:string(i))
-  end
+  screen.move(0, o + 10)
+  screen.text(m.haha)
+
+  --for k,i in pairs(m.params:list()) do
+  --    screen.move(  0, o + 10 * i)
+  --    screen.text(m.params:get_name(i))
+  --    screen.move(127, o + 10 * i)
+  --    screen.text_right(m.params:string(i))
+  --end
 
   screen.update()
 end
@@ -248,6 +243,14 @@ end
 m.init = function()
     -- on menu entry, ie, if you wanted to start timers
     state.x = 0
+
+    local paramset = require 'core/paramset'
+
+    m.params = paramset.new("modmenu", "Mod Menu")
+    m.params:add_number("a", "num widgets", 3, 7, 5)
+    m.params:add_number("b", "num buckets", 12, 24, 18)
+
+    m.haha = "hoho"
 end
 m.deinit = function()
     -- on menu exit
